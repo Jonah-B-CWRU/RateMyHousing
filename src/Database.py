aa1d1977b5611634ab35804e4f2ccfc39fd23a24
Aaron Brey (script) <aabrey321@gmail.com>
2026-02-09 00:29:59 -0500

    made private genaric doc to id function more specific relations in progress

--- src/Database.py
+++ src/Database.py
@@ -2,6 +2,7 @@ from typing import Any
 from firebase_admin import firestore, credentials
 from google.cloud.firestore_v1.client import Client as FirestoreClient
 from google.cloud.firestore_v1.document import DocumentReference
+from google.cloud.firestore_v1.query import Query
 from dataclasses import dataclass, asdict
 from google.protobuf import timestamp_pb2
 import firebase_admin
@@ -34,7 +35,7 @@ class Listing:
 
 @dataclass
 class Rating:
-    ratingID: str
+    RatingID: str
     UserID: str
     Rating: int
     def as_dict(self) -> dict:
@@ -134,7 +135,7 @@ class database_manager:
     # Landloard
     def get_landlords(self):
         if self.connected:
-            return self._get_data("Listing")
+            return self._get_data("Landlord")
         else:
             raise IOError("Not Connected")
 
@@ -153,3 +154,66 @@ class database_manager:
         if self.connected:
             result = self._push_data(rating.as_dict(),"Rating")
 
+
+    # relating stuff
+    def _unwrap_query(self, q:Query) -> list[dict]:
+        total_data:list[dict] = []
+        for doc in q.get():
+            data = doc.to_dict()
+            if data != None:
+                total_data.append(data)
+        return total_data
+    
+    def _get_document_using_id(self,collection_to_search:str, Id_source:User|Rating|Landlord|Password|Comments|Listing)-> list[dict]:
+        """
+        Searches the specified collection for all documents with the corresponding ID.
+        
+        Parameters:
+            collection_to_search (str): The name of the collection to search within.
+            Id_source (User|Rating|Landlord|Password|Comments|Listing): The document containing the ID to search for.
+
+        Returns:
+            list: A list of documents that match the specified ID.
+        """
+        if self.connected:
+            collection = self.fire_store.collection(collection_to_search)
+            match type(Id_source):
+                case User():
+                    user:User = Id_source
+                    id = user.UserID
+                    query = collection.where("UserID",id)
+                    return self._unwrap_query(query)
+                case Password():
+                    password:Password = Id_source
+                    id = password.UserID
+                    query = collection.where("UserID",id)
+                    return self._unwrap_query(query)
+                case Rating():
+                    rating:Rating = Id_source
+                    id = rating.RatingID
+                    query = collection.where("RatingID",id)
+                    return self._unwrap_query(query)
+                case Landlord():
+                    landlord:Landlord = Id_source
+                    id = landlord.LLID
+                    query = collection.where("LLID",id)
+                    return self._unwrap_query(query)
+                case Comments():
+                    comments:Comments = Id_source
+                    id = comments.CommentId
+                    query = collection.where("CommentId",id)
+                    return self._unwrap_query(query)
+                case Listing():
+                    listing:Listing = Id_source
+                    id = listing.ListingID
+                    query = collection.where("ListingID",id)
+                    return self._unwrap_query(query)
+                case _:
+                    raise TypeError("Id_source Not Valid Type")
+        raise IOError("Not Connected to Database")
+
+    def get_pass_from_user(self, user:User) -> Password:
+        if self.connected:
+            
+            pass
+        raise IOError("Not Connected to Database")
