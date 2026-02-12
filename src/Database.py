b3d808524e6ecfcbad543dc060193e605bf42646
Aaron Brey <129773617+Error401-UNF@users.noreply.github.com>
2026-02-12 01:21:01 -0400

    Data relating (#1)
    * made private genaric doc to id function more specific relations in progress
    
    * change readme and slightly rework data-relating helper function
    
    * finished all data relating functions

--- src/Database.py
+++ src/Database.py
@@ -2,6 +2,7 @@ from typing import Any
 from firebase_admin import firestore, credentials
 from google.cloud.firestore_v1.client import Client as FirestoreClient
 from google.cloud.firestore_v1.document import DocumentReference
+from google.cloud.firestore_v1.query import Query
 from dataclasses import dataclass, asdict
 from google.protobuf import timestamp_pb2
 import firebase_admin
@@ -9,42 +10,44 @@ import firebase_admin
 # all dataclasses's for easy use
 @dataclass
 class Comments:
-    CommentId: str
-    ConnectedCommentID: str
-    ListingID: str
-    Content: str
+    CommentId: str = ""
+    ConnectedCommentID: str = ""
+    ListingID: str = ""
+    UserID:str = ""
+    Content: str = ""
     def as_dict(self) -> dict:
         return asdict(self)
 
 @dataclass
 class Landlord:
-    LLID: str
-    Name: str
-    Email: str
+    LLID: str = ""
+    Name: str = ""
+    Email: str = ""
     def as_dict(self) -> dict:
         return asdict(self)
 
 @dataclass
 class Listing:
-    ListingID: str
-    LLID: str
-    ListingLocation: str
+    ListingID: str = ""
+    LLID: str = ""
+    ListingLocation: str = ""
     def as_dict(self) -> dict:
         return asdict(self)
 
 @dataclass
 class Rating:
-    ratingID: str
-    UserID: str
-    Rating: int
+    RatingID: str = ""
+    UserID: str = ""
+    ListingID: str = ""
+    Rating: int = 0
     def as_dict(self) -> dict:
         return asdict(self)
 
 @dataclass
 class Password:
-    Hash: str
-    Salt: str
-    UserID: str
+    Hash: str = ""
+    Salt: str = ""
+    UserID: str = ""
     def as_dict(self) -> dict:
         return asdict(self)
     
@@ -134,7 +137,7 @@ class database_manager:
     # Landloard
     def get_landlords(self):
         if self.connected:
-            return self._get_data("Listing")
+            return self._get_data("Landlord")
         else:
             raise IOError("Not Connected")
 
@@ -153,3 +156,235 @@ class database_manager:
         if self.connected:
             result = self._push_data(rating.as_dict(),"Rating")
 
+
+    # relating stuff
+    def _unwrap_query(self, q:Query) -> list[dict]:
+        total_data:list[dict] = []
+        for doc in q.get():
+            data = doc.to_dict()
+            if data != None:
+                total_data.append(data)
+        return total_data
+    
+    def _get_document_using_id(self,collection_to_search:str, Id_type:User|Rating|Landlord|Password|Comments|Listing, Id_to_look_for:str)-> list[dict]:
+        """
+        Searches the specified collection for all documents with the corresponding ID.
+        
+        Parameters:
+            collection_to_search (str): The name of the collection to search within.
+            Id_type (User|Rating|Landlord|Password|Comments|Listing): The document containing the ID to search for.
+            Id_to_look_for (str): exact text of cid to look for
+
+        Returns:
+            list: A list of documents that match the specified ID.
+        """
+        if self.connected:
+            collection = self.fire_store.collection(collection_to_search)
+            match Id_type:
+                case User():
+                    query = collection.where("UserID", "==", Id_to_look_for)
+                    return self._unwrap_query(query)
+                    
+                case Password():
+                    query = collection.where("UserID", "==", Id_to_look_for)
+                    return self._unwrap_query(query)
+                    
+                case Rating():
+                    query = collection.where("RatingID", "==", Id_to_look_for)
+                    return self._unwrap_query(query)
+                    
+                case Landlord():
+                    query = collection.where("LLID", "==", Id_to_look_for)
+                    return self._unwrap_query(query)
+                    
+                case Comments():
+                    query = collection.where("CommentId", "==", Id_to_look_for)
+                    return self._unwrap_query(query)
+                    
+                case Listing():
+                    query = collection.where("ListingID", "==", Id_to_look_for)
+                    return self._unwrap_query(query)
+                    
+                case _:
+                    raise TypeError(f"Id_source Not Valid Type: {Id_type}, {type(Id_type)}")
+        raise IOError("Not Connected to Database")
+    
+    def check_for_username(self, username:str) -> bool:
+        if self.connected:
+            collection = self.fire_store.collection("Users")
+            query = collection.where("Username","==",username)
+            user = self._unwrap_query(query)
+            if len(user) >= 1:
+                return True
+            else:
+                return False
+        raise IOError("Not Connected to Database")
+   
+    def check_for_email(self, email:str) -> bool:
+        if self.connected:
+            collection = self.fire_store.collection("Users")
+            query = collection.where("Email","==",email)
+            user = self._unwrap_query(query)
+            if len(user) >= 1:
+                return True
+            else:
+                return False
+        raise IOError("Not Connected to Database")
+
+
+    # User relations
+    # username -> user
+    # user -> password
+    # User <-> lanloard
+    # user <-> rating (many)
+    def get_user_with_username(self, username:str) -> User:
+        if self.connected:
+            collection = self.fire_store.collection("Users")
+            query = collection.where("Username","==",username)
+            user = self._unwrap_query(query)
+            if len(user) == 1:
+                user = user[0]
+                return User(user["UserID"],user["Username"],user["ConnectedLL"],user["Email"])
+            else:
+                raise TypeError(f"no user with the username: {username}")
+        raise IOError("Not Connected to Database")
+
+    def get_pass_from_user(self, user:User) -> Password:
+        if self.connected:
+            password_list = self._get_document_using_id("Passwords",User(),user.UserID)
+            if len(password_list) == 1:
+                p = password_list[0]
+                return Password(p["Hash"],p["Salt"],p["UserID"])
+            else:
+                raise TypeError(f"no password with userid: {user.UserID}")
+        raise IOError("Not Connected to Database")
+    
+    def get_lanloard_from_user(self, user:User) -> Landlord:
+        if self.connected:
+            Landlord_list = self._get_document_using_id("Lanloards", Landlord(),user.ConnectedLL)
+            if len(Landlord_list) == 1:
+                l = Landlord_list[0]
+                return Landlord(l["LLID"],l["Name"],l["Email"])
+            else:
+                raise TypeError(f"no Lanloard with LLID: {user.ConnectedLL}")
+        raise IOError("Not Connected to Database")
+
+    def get_ratings_from_user(self, user:User) -> list[Rating]:
+        if self.connected:
+            ratings = self._get_document_using_id("Ratings", User(),user.UserID)
+            out:list[Rating] = []
+            for r in ratings:
+                out.append(Rating(r["RatingID"],r["UserID"],r["ListingID"],r["Rating"]))
+            return out
+        raise IOError("Not Connected to Database")
+    
+    # rating relationships
+    # Rating <-> user
+    # Rating <-> Listing
+    def get_user_from_rating(self,rating:Rating) -> User:
+        if self.connected:
+            users = self._get_document_using_id("User",User(),rating.UserID)
+            if len(users) == 1:
+                u = users[0]
+                return User(u["UserID"],u["Username"],u["ConnectedLL"],u["Email"])
+            else:
+                raise TypeError(f"no username with userid: {rating.UserID}")
+        raise IOError("Not Connected to Database")
+    
+    def get_listing_from_rating(self,rating:Rating) -> Listing:
+        if self.connected:
+            listings = self._get_document_using_id("Listing",Listing(),rating.ListingID)
+            if len(listings) == 1:
+                l = listings[0]
+                return Listing(l["ListingID"],l["LLID"],l["ListingLocation"])
+            else:
+                raise TypeError(f"no listing with ListingID: {rating.ListingID}")
+        raise IOError("Not Connected to Database")
+    
+    # comments relationships
+    # comment -> user
+    # comment -> comment
+    # comment <-> listing
+    def get_user_from_comments(self,comment:Comments) -> User:
+        if self.connected:
+            users = self._get_document_using_id("User",User(),comment.UserID)
+            if len(users) == 1:
+                u = users[0]
+                return User(u["UserID"],u["Username"],u["ConnectedLL"],u["Email"])
+            else:
+                raise TypeError(f"no username with userid: {comment.UserID}")
+        raise IOError("Not Connected to Database")
+    
+    def get_comments_from_comments(self,comment:Comments) -> Comments:
+        if self.connected:
+            com = self._get_document_using_id("Comments",Comments(),comment.ConnectedCommentID)
+            if len(com) == 1:
+                c = com[0]
+                return Comments(c["CommentId"],c["ConnectedCommentID"],c["ListingID"],c["UserID"],c["Content"])
+            else:
+                raise TypeError(f"no comment with comment: {comment.ConnectedCommentID}")
+        raise IOError("Not Connected to Database")
+    
+    def get_listing_from_comments(self,comment:Comments) -> Listing:
+        if self.connected:
+            listings = self._get_document_using_id("Listing",Listing(),comment.ListingID)
+            if len(listings) == 1:
+                l = listings[0]
+                return Listing(l["ListingID"],l["LLID"],l["ListingLocation"])
+            else:
+                raise TypeError(f"no listing with ListingID: {comment.ListingID}")
+        raise IOError("Not Connected to Database")
+    
+
+    # Listing relationships
+    # listing -> landlord
+    # listing <-> Rating (many)
+    # listing <-> Comments (many)
+    def get_lanloard_from_Listing(self, listing:Listing) -> Landlord:
+        if self.connected:
+            Landlord_list = self._get_document_using_id("Lanloards", Landlord(),listing.LLID)
+            if len(Landlord_list) == 1:
+                l = Landlord_list[0]
+                return Landlord(l["LLID"],l["Name"],l["Email"])
+            else:
+                raise TypeError(f"no Lanloard with LLID: {listing.LLID}")
+        raise IOError("Not Connected to Database")
+    
+    def get_ratings_from_listing(self, listing:Listing) -> list[Rating]:
+        if self.connected:
+            ratings = self._get_document_using_id("Ratings", Listing(),listing.ListingID)
+            out:list[Rating] = []
+            for r in ratings:
+                out.append(Rating(r["RatingID"],r["UserID"],r["ListingID"],r["Rating"]))
+            return out
+        raise IOError("Not Connected to Database")
+    
+    def get_comments_from_listing(self, listing:Comments) -> list[Comments]:
+        if self.connected:
+            coms = self._get_document_using_id("Comments", Listing(),listing.ListingID)
+            out:list[Comments] = []
+            for c in coms:
+                out.append(Comments(c["CommentId"],c["ConnectedCommentID"],c["ListingID"],c["UserID"],c["Content"]))
+            return out
+        raise IOError("Not Connected to Database")
+    
+    # landlord rlationships
+    # Landlord <-> User (many)
+    # Landlord <-> Listing (many)
+    def get_connected_users_with_landlord(self, landlord:Landlord) -> list[User]:
+        if self.connected:
+            users = self._get_document_using_id("User", Landlord(),landlord.LLID)
+            out:list[User] = []
+            for u in users:
+                out.append(User(u["UserID"],u["Username"],u["ConnectedLL"],u["Email"]))
+            return out
+        raise IOError("Not Connected to Database")
+    
+    def get_connected_listings_with_landlord(self, landlord:Landlord) -> list[Listing]:
+        if self.connected:
+            listings = self._get_document_using_id("Listing", Landlord(),landlord.LLID)
+            out:list[Listing] = []
+            for l in listings:
+                out.append(Listing(l["ListingID"],l["LLID"],l["ListingLocation"]))
+            return out
+        raise IOError("Not Connected to Database")
